27738ff12d307cddd363a0ed056482748934b17d
L <changlifang@gmail.com>
2019-08-02 14:33:08 -0700

    chapter1

---
+++ binarySearch1.py
@@ -0,0 +1,22 @@
+def binarySearch(list, item):
+	''' search the list for the target item
+	    if found, return the index, i.e. the position in the my_list
+	    otherwise, return None
+	'''
+	#your code
+
+	return None
+
+
+
+
+import unittest
+class TestBinarySearch(unittest.TestCase):
+	def test1(self):
+		my_list = [1,3,5,7,9,11]
+		#self.assertEqual(binarySearch(my_list, 3), 1)
+		self.assertEqual(binarySearch(my_list, 2), None)
+
+
+if __name__ == '__main__':
+	unittest.main()
