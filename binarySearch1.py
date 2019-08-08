7cc0d0d852d2fe2281d4ad74cc709e446ba91879
L <changlifang@gmail.com>
2019-08-07 17:07:33 -0700

    bin search

--- binarySearch1.py
+++ binarySearch1.py
@@ -4,8 +4,21 @@ def binarySearch(list, item):
 	    otherwise, return None
 	'''
 	#vincent's code
-
+	
+	low=0
+	high=len(list)-1
+	
+	while low <= high:
+		mid=(low+high)//2
+		if list[mid] > item:
+			high=mid-1
+		elif list[mid]==item:
+			return mid
+		else:
+			low=mid+1
 	return None
+
+	
 
 
 
@@ -14,7 +27,7 @@ import unittest
 class TestBinarySearch(unittest.TestCase):
 	def test1(self):
 		my_list = [1,3,5,7,9,11]
-		#self.assertEqual(binarySearch(my_list, 3), 1)
+		self.assertEqual(binarySearch(my_list, 3), 1)
 		self.assertEqual(binarySearch(my_list, 2), None)
 
 
